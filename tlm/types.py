0d41f7b6a26dffc2668c4c867da7793e5396d041
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2024-07-04 08:49:07 +0100

    feat(types): define `Scalar` and `Parameters` types

--- tlm/types.py
+++ tlm/types.py
@@ -10,12 +10,18 @@ __all__ = [
     "Batched",
     "Boolean",
     "Matrix",
+    "Parameters",
+    "Scalar",
     "Vector",
 ]
 
 # Custom base arrays
+type Scalar = Annotated[jax.Array, tuple[()]]
 type Vector = Annotated[jax.Array, tuple[int]]
 type Matrix = Annotated[jax.Array, tuple[int, int]]
+
+# Type to the parameters in the model
+type Parameters = dict[str, jax.Array]
 
 # Annotation for arrays containing a first batch dimension
 type Batched[T] = Annotated[T, "batched first dimension"]
